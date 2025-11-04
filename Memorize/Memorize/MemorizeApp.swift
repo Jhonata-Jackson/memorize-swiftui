aa2bfbe8ae79b60c319d8d5a1b99415b6a4efdf9
Jhonata Motta <tqi_jmotta@uolinc.com>
2025-11-04 20:49:48 -0300

    feat: :sparkles: upload the base project

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jhonata Jackson Monteiro Motta on 04/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
